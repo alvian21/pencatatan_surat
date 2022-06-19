8c308d6ec609d225951c3dbdb91334dc31aea49d
Alvian Ardhiansyah <alvian.ardhiansyah@gmail.com>
2022-06-19 11:19:22 +0700

    revisi

---
+++ database/migrations/2022_06_19_110949_create_question_answers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_answers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('question_id')->unsigned()->index()->nullable();
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+            $table->string('status');
+            $table->string('name');
+            $table->double('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_answers');
+    }
+}
